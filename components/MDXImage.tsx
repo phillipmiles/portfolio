97e73419e8d265dbcf65028dbc310d33ff6eb63a
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-10-03 01:46:20 +1100

    Worked on MDX Image

--- components/MDXImage.tsx
+++ components/MDXImage.tsx
@@ -1,22 +1,69 @@
 import Image from 'next/image';
 
-const MDXImage = ({ alt, priority, src }): JSX.Element => (
-  <div
-    style={{
-      height: '512px',
-      overflow: 'hidden',
-      position: 'relative',
-      marginBottom: '96px',
-    }}
-  >
-    <Image
-      src={src}
-      alt={alt}
-      fill={true}
-      style={{ objectFit: 'cover' }}
-      priority={priority}
-    />
-  </div>
-);
+const MDXImage = ({
+  alt,
+  priority,
+  src,
+  wide,
+  float,
+  width,
+  height,
+  aspectRatio = '16 / 9',
+}): JSX.Element => {
+  const w = `${width}px`;
+  const h = `${height}px`;
+  console.log(width, height);
+  return (
+    <div
+      className="mdx-content-image"
+      style={{
+        overflow: 'hidden',
+        position: 'relative',
+        maxWidth: w,
+        margin: 'auto',
+        marginTop: '48px',
+        marginBottom: '48px',
+        aspectRatio: aspectRatio,
+        width: '100%',
+
+        ...(wide && {
+          maxWidth: '100%',
+        }),
+
+        ...(float === 'right' && {
+          marginTop: 0,
+          // marginBottom: 0,
+          float: 'right',
+          marginLeft: '48px',
+          clear: 'both',
+        }),
+      }}
+    >
+      {/* <div
+        style={{
+          height: h,
+          overflow: 'hidden',
+          position: 'relative',
+          // aspectRatio: '1 / 1',
+          // height: 'auto',
+          // overflow: 'hidden',
+          // position: 'relative',
+        }}
+      > */}
+      <Image
+        src={src}
+        alt={alt}
+        // width={width}
+        // height={height}
+        // width={!wide || float ? width : undefined}
+        // height={height}
+        fill={true}
+        style={{ objectFit: 'cover' }}
+        priority={priority}
+      />
+      {/* </div> */}
+    </div>
+  );
+};
 
 export default MDXImage;
