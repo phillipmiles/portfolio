a1464c9b33e11434687a73f34d2ee3d87eeb3bfa
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-10-05 14:33:59 +1100

    Added MDXGallery component

---
+++ components/MDXGallery.tsx
@@ -0,0 +1,141 @@
+import Image from 'next/image';
+
+const MDXGallery = ({
+  alt,
+  priority,
+  images,
+  wide,
+  float,
+  width,
+  height,
+  aspectRatio = '16 / 9',
+}): JSX.Element => {
+  const w = `${width}px`;
+  const h = `${height}px`;
+  return (
+    <div
+      style={{
+        marginTop: '48px',
+        marginBottom: '48px',
+        display: 'flex',
+        flexDirection: 'row',
+        height: '540px',
+      }}
+    >
+      <div
+        style={{
+          overflow: 'hidden',
+          position: 'relative',
+          width: '65%',
+          aspectRatio: aspectRatio,
+          backgroundColor: '#DDDDDD',
+          marginRight: '16px',
+        }}
+      >
+        {/* <div
+        style={{
+          height: h,
+          overflow: 'hidden',
+          position: 'relative',
+          // aspectRatio: '1 / 1',
+          // height: 'auto',
+          // overflow: 'hidden',
+          // position: 'relative',
+        }}
+      > */}
+        <Image
+          src={images[0].src}
+          alt={images[0].alt}
+          // width={width}
+          // height={height}
+          // width={!wide || float ? width : undefined}
+          // height={height}
+          fill={true}
+          style={{ objectFit: 'cover' }}
+          priority={priority}
+        />
+        {/* </div> */}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          // width: '35%'
+          width: '416px',
+        }}
+      >
+        <div
+          style={{
+            overflow: 'hidden',
+            position: 'relative',
+            height: '50%',
+            marginBottom: '8px',
+            flexGrow: 1,
+            width: '100%',
+            backgroundColor: '#DDDDDD',
+          }}
+        >
+          {/* <div
+        style={{
+          height: h,
+          overflow: 'hidden',
+          position: 'relative',
+          // aspectRatio: '1 / 1',
+          // height: 'auto',
+          // overflow: 'hidden',
+          // position: 'relative',
+        }}
+      > */}
+          <Image
+            src={images[1].src}
+            alt={images[1].alt}
+            // width={width}
+            // height={height}
+            // width={!wide || float ? width : undefined}
+            // height={height}
+            fill={true}
+            style={{ objectFit: 'cover' }}
+            priority={priority}
+          />
+          {/* </div> */}
+        </div>
+        <div
+          style={{
+            overflow: 'hidden',
+            position: 'relative',
+            marginTop: '8px',
+            height: '50%',
+            width: '100%',
+            backgroundColor: '#DDDDDD',
+          }}
+        >
+          {/* <div
+        style={{
+          height: h,
+          overflow: 'hidden',
+          position: 'relative',
+          // aspectRatio: '1 / 1',
+          // height: 'auto',
+          // overflow: 'hidden',
+          // position: 'relative',
+        }}
+      > */}
+          <Image
+            src={images[2].src}
+            alt={images[2].alt}
+            // width={width}
+            // height={height}
+            // width={!wide || float ? width : undefined}
+            // height={height}
+            fill={true}
+            style={{ objectFit: 'cover' }}
+            priority={priority}
+          />
+          {/* </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MDXGallery;
