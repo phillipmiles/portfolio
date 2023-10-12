c44e9c916d02f1b3e5193ee3801ed83a4bc6fc1c
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-10-12 18:54:07 +1100

    Fixed sticky top bar

--- components/generic/StickyTopBar.tsx
+++ components/generic/StickyTopBar.tsx
@@ -1,36 +1,39 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 const StickyTopBar = ({ children }) => {
-  const prevScrollY = useRef(0);
+  // const prevScrollY = useRef(0);
 
   const headerRef = useRef(null);
   const [headerTranslate, setHeaderTranslate] = useState(0);
+  const [prevScrollY, setPrevScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = (e) => {
       const headerBoundingRect = headerRef.current.getBoundingClientRect();
       const currentScrollY = window.scrollY;
       const distanceFromTop = currentScrollY + headerBoundingRect.top;
-
+      console.log('BAM');
       setHeaderTranslate((oldState) => {
-        let newTranslate =
-          oldState - (prevScrollY.current - currentScrollY) * -1;
+        console.log('BONG');
+        let newTranslate = oldState - (prevScrollY - window.scrollY) * -1;
+        console.log(newTranslate);
 
         if (
           headerBoundingRect.top <= 0
           // &&
           // prevScrollY.current <= currentScrollY
         ) {
-          if (headerBoundingRect.height * -1 > newTranslate) {
+          if (headerBoundingRect.height * -1 >= newTranslate) {
             console.log('hi');
             return headerBoundingRect.height * -1;
           } else if (0 < newTranslate) {
             console.log('hi3', newTranslate);
             return 0;
           } else {
+            console.log(newTranslate);
             return newTranslate;
           }
-        } else if (prevScrollY.current > currentScrollY) {
+        } else if (prevScrollY > currentScrollY) {
           console.log('nope2');
           return oldState;
         } else {
@@ -48,13 +51,16 @@ const StickyTopBar = ({ children }) => {
         //   return newTranslate;
         // }
       });
-      prevScrollY.current = currentScrollY;
+
+      setPrevScrollY(window.scrollY);
     };
 
     window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [headerTranslate]);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollY]);
 
   return (
     <header
