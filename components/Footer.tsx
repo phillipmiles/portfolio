f1f07deb0d2cb5e19692639b96b9f48a8a163ae9
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-10-19 14:08:38 +1100

    Added responsive footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,39 +4,18 @@ import Flex from './generic/Flex';
 import PageContentWrap from './generic/PageContentWrap';
 import s from './Footer.module.css';
 
-const Footer = (props): JSX.Element => {
+const Footer = ({ ...props }): JSX.Element => {
   return (
-    <footer className={s.footer} style={{ backgroundColor: '#262C35' }}>
-      <PageContentWrap
-        style={{
-          color: 'white',
-          paddingTop: 64,
-          paddingBottom: 64,
-        }}
-      >
-        <Flex
-          style={{
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            flexDirection: ['column', 'row'],
-          }}
-        >
-          <Flex
-            style={{
-              minWidth: 240,
-              alignItems: 'center',
-              // mb: [4, 0],
-            }}
-          >
+    <footer className={s.footer} {...props}>
+      <PageContentWrap classNameInner={s.footerPageWrap}>
+        <Flex className={s.footerContent}>
+          <Flex className={s.avatarContent}>
             <Image
               src={'/images/avatar.jpg'}
               height={64}
               width={64}
               alt=""
-              style={{
-                borderRadius: 64,
-                marginRight: 24,
-              }}
+              className={s.avatar}
             />
             <Flex style={{ flexDirection: 'column' }}>
               <p
@@ -48,13 +27,10 @@ const Footer = (props): JSX.Element => {
                 My name is
               </p>
               <Link
-                // to={routeUrls.about}
                 href="/"
-                // variant="callout"
                 style={{
                   color: 'white',
                   fontSize: '22px',
-                  // ':hover': { textDecoration: 'underline' }
                 }}
               >
                 Phillip Miles
@@ -62,66 +38,15 @@ const Footer = (props): JSX.Element => {
             </Flex>
           </Flex>
 
-          <Flex
-            style={{
-              // width: '33%',
-              minWidth: 240,
-              // flexDirection: ['row', 'column'],
-              flexDirection: 'column',
-              justifyContent: 'center',
-            }}
-          >
-            <p
-              style={{
-                color: '#59b3ff',
-                marginBottom: 0,
-                // variant: ['text.detail', 'text.body'],
-                // mx: [2, 0],
-              }}
-            >
-              Follow me on Twitter
-            </p>
-            <Link
-              href="https://twitter.com/PhillipAMiles"
-              // variant="detail"
-              className="small"
-              style={{
-                // mx: [1, 0],
-
-                color: 'white',
-                // ':hover': { textDecoration: 'underline' },
-              }}
-            >
+          <Flex className={s.linkContent}>
+            <p className={s.linkLabel}>Follow me on Twitter</p>
+            <Link href="https://twitter.com/PhillipAMiles" className={s.link}>
               @PhillipAMiles
             </Link>
           </Flex>
-          <Flex
-            style={{
-              // width: '33%',
-              minWidth: 240,
-              // flexDirection: ['row', 'column'],
-              flexDirection: 'column',
-              justifyContent: 'center',
-            }}
-          >
-            <p
-              style={{
-                // color: 'neutral.6',
-                color: '#59b3ff',
-                // variant: ['text.detail', 'text.body'],
-                // mx: [2, 0],
-                marginBottom: 0,
-              }}
-            >
-              Get in touch
-            </p>
-            <Link
-              href="mailTo:hello@phillipmiles.com"
-              className="small"
-              style={{
-                color: 'white',
-              }}
-            >
+          <Flex className={s.linkContent}>
+            <p className={s.linkLabel}>Get in touch</p>
+            <Link href="mailTo:hello@phillipmiles.com" className={s.link}>
               hello@phillipmiles.com
             </Link>
           </Flex>
