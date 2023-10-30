c94b06ac9ef03fa9bacf91cebdf739ea4484c3e6
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-10-30 16:11:31 +1100

    Panner now works correctly if screen is resized

--- components/generic/InfinityPanner.tsx
+++ components/generic/InfinityPanner.tsx
@@ -38,19 +38,8 @@ const InfinityPanner = ({
   translateY = 0,
   ...props
 }: Props): JSX.Element => {
-  const [isTransitioning, setIsTransitioning] = useState(false);
-  // const intervalRef = useRef();
   const containerRef = useRef<HTMLInputElement>(null);
   const [appendElements, setAppendElements] = useState([]);
-  // useEffect(() => {
-  //   setIsTransitioning(true);
-  // }, []);
-
-  // useInterval(() => {
-  //   console.log('her');
-  //   if (!containerRef.current) return;
-  //   console.log('made it');
-  // }, 1000);
 
   // LOOKAT THIS
   // https://www.michellemcquaid.com/
@@ -61,125 +50,52 @@ const InfinityPanner = ({
   // Only slide the container not the individual items
 
   useEffect(() => {
-    const onInterval = () => {
+    const recalcPanner = () => {
       if (!containerRef.current) return;
-      const containerBoundingBox = containerRef.current.getBoundingClientRect();
 
-      let left = 0;
-      for (const child of containerRef.current.children) {
-        console.log(left);
-        const childBoundingBox = child.getBoundingClientRect();
-        child.style.left = `${left}px`;
-        // if(childBoundingBox.length)
-        // child.style.transform = `translateX(-${containerBoundingBox.width}px)`;
-        if (childBoundingBox.left < 0) {
-          child.style.transform = `translateX(${
-            containerBoundingBox.width * 2
-          }px)`;
-          child.style.transitionDuration = '0ms';
-          // containerRef.current.appendChild(child);
-        } else {
-          child.style.transitionDuration = '5000ms';
-          child.style.transform = `translateX(-${
-            containerBoundingBox.width * 2
-          }px)`;
-        }
+      const items = containerRef.current.children;
 
-        left = left + childBoundingBox.width;
-        // console.log(left);
+      let width = 0;
+
+      for (let index = 0; index < children.length; index++) {
+        console.log('run');
+        width = width + items[index].getBoundingClientRect().width;
+        width = width + parseInt(items[index].style.marginLeft);
+        width = width + parseInt(items[index].style.marginRight);
       }
-      // containerRef.current.children.map((child) => {
-      //   const boundingRect = child.getBoundingClientRect();
-      //   console.log(boundingRect);
-      // });
+
+      const append = [];
+
+      let appendWidth = 0;
+      for (let index = 0; index < children.length; index++) {
+        if (appendWidth > containerRef.current.getBoundingClientRect().width)
+          break;
+        const el = containerRef.current.children[index];
+        appendWidth = appendWidth + el.getBoundingClientRect().width;
+        appendWidth = appendWidth + parseInt(el.style.marginLeft);
+        appendWidth = appendWidth + parseInt(el.style.marginRight);
+        append.push(children[index]);
+      }
+
+      setAppendElements(append);
+
+      containerRef.current.style.transform = `translateX(${
+        (width - 1) * translateX
+      }px)`;
     };
 
-    // let intervalRef = setInterval(onInterval, 10);
+    onresize = recalcPanner;
 
-    // return () => {
-    //   if (intervalRef) {
-    //     clearInterval(intervalRef);
-    //   }
-    // };
-  }, []);
+    recalcPanner();
+    document.addEventListener('resize', onresize);
 
-  const addChildren = () => {
-    let leftPosition = 0;
-
-    return children.map((child, index) => {
-      // leftPosition = leftPosition + child.getBoundingClientRect().width;
-      // console.log(child);
-      // child.props.style = { ...child.props.style, backgroundColor: 'red' };
-      return (
-        <div
-          key={index}
-          style={{
-            // position: 'absolute',
-            // float: 'left',
-            // left: `${leftPosition}px`,
-            // transition: 'all 5000ms',
-            // transform: isTransitioning ? 'translateX(-100%)' : 'translateX(0%)',
-            transitionProperty: 'transform',
-            transitionTimingFunction: 'linear',
-          }}
-        >
-          {child}
-        </div>
-      );
-    });
-  };
-
-  useEffect(() => {
-    if (!containerRef.current) return;
-
-    const items = containerRef.current.children;
-
-    let width = 0;
-    for (const item of items) {
-      width = width + item.getBoundingClientRect().width;
-      width = width + parseInt(item.style.marginLeft);
-      width = width + parseInt(item.style.marginRight);
-    }
-
-    console.log(width);
-    console.log(containerRef.current.getBoundingClientRect().width);
-    // console.log(width - containerRef.current.getBoundingClientRect().width);
-    const remainder = width;
-    console.log(remainder);
-
-    // setAppendElements([containerRef.current.children[0].cloneNode()]);
-
-    const append = [];
-
-    let appendWidth = 0;
-    for (let index = 0; index < children.length; index++) {
-      if (appendWidth > containerRef.current.getBoundingClientRect().width)
-        break;
-      const el = containerRef.current.children[index];
-      appendWidth = appendWidth + el.getBoundingClientRect().width;
-      appendWidth = appendWidth + parseInt(el.style.marginLeft);
-      appendWidth = appendWidth + parseInt(el.style.marginRight);
-      append.push(children[index]);
-    }
-    console.log(appendWidth);
-    setAppendElements(append);
-
-    // width =
-    //   width + appendWidth - containerRef.current.getBoundingClientRect().width;
-    containerRef.current.style.transform = `translateX(${
-      (width - 1) * translateX
-    }px)`;
-    // containerRef.current.children.reduce((item) => {
-    //   return item.getBoundingClientBox().width;
-    // });
-    // console.log(containerRef.current.children[0]);
-    // containerRef.current.append(containerRef.current.children[0].cloneNode());
-
-    // containerRef.current.append(containerRef.current.children[1].cloneNode());
-    // containerRef.current.append(containerRef.current.children[2].cloneNode());
-  }, []);
+    return () => {
+      document.removeEventListener('resize', onresize);
+    };
+  }, [children, translateX]);
 
   console.log(appendElements);
+
   return (
     <div
       className={`${s.container} ${className}`}
