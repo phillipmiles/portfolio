37725ad67313a333beb54dc9c665be598f508cd6
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2023-12-01 18:52:14 +1100

    I guess fixing of folder name case that git was ignoring

---
+++ data/tools/component-library/slider/index.js
@@ -0,0 +1,18 @@
+import { code, Example } from './example_01';
+
+const examples = {
+  slug: 'slider',
+  title: 'Slider',
+  category: 'Inputs',
+  description:
+    "A simple utility component that replaces a div with 'display: flex' and 'flex-direction: column' to a component called Flex. I use flex alot and seeing a Flex component when scanning code is just easier for me to read.",
+  content: [
+    {
+      type: 'text',
+      example: Example,
+      exampleMarkDown: code,
+    },
+  ],
+};
+
+export default examples;
