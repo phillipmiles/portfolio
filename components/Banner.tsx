aaf6cd528f6c9bad5eae1417654fa0c4e724b744
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2024-04-15 14:10:42 +1000

    Restricted with width of the nav banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
 import s from './Banner.module.css';
 import Nav from './Nav';
-import StickyTopBar from './generic/StickyTopBar';
 
 interface Props {
   invert?: boolean;
 }
 
 const Banner = ({ invert }: Props): JSX.Element => (
-  // <StickyTopBar>
-  <div className={`${s.banner} ${invert && s.invert}`}>
-    <div className={s.logo}>
-      <Link href="/">Phillip Miles</Link>
+  <div className={s.bannerContainer}>
+    <div className={`${s.banner} ${invert && s.invert}`}>
+      <div className={s.logo}>
+        <Link href="/">Phillip Miles</Link>
+      </div>
+      <Nav invert={invert} />
     </div>
-    <Nav invert={invert} />
   </div>
-  // </StickyTopBar>
 );
 
 export default Banner;
